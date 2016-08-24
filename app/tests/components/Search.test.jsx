fbce9b2ba4bb7a7d396a1970683ac405acddc90c
Jacob Carvell <jacobcarvell@gmail.com>
2016-08-24 15:42:00 +1000

    Added testing for the search component

---
+++ app/tests/components/Search.test.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Search = require('Search');
+
+describe('Search', () => {
+  it('Should exist', () => {
+    expect(Search).toExist();
+  });
+
+  it('Should call onSearch with input text', () => {
+    var searchText = 'jacob';
+    var spy = expect.createSpy();
+    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+
+    search.refs.searchText.value = searchText;
+    TestUtils.Simulate.change(search.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, searchText);
+  });
+
+  it("Should call onSearch with proper checked value", () => {
+    var spy = expect.createSpy();
+    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+
+    search.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(search.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+  });
+
+});
