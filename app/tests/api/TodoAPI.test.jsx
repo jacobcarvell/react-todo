ce5237cc34359ba668b5ae209e21b252e618e379
Jacob Carvell <jacobcarvell@gmail.com>
2016-08-25 12:25:02 +1000

    Added search functionality to the form

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -41,4 +41,37 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {id: 1, text: 'test 1', completed: true},
+      {id: 2, text: 'test 2', completed: false},
+      {id: 3, text: 'something else', completed: true}
+    ]
+
+    it('Should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('Should return non completed items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('Should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toEqual(false);
+    });
+
+    it('Should filter todos by searchtext', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'Test');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('Should return all todos if searchtext if empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
