a0c2451030247dbfad84c3e8906d84b44a09eab2
Jacob Carvell <jacobcarvell@gmail.com>
2016-08-25 21:09:58 +1000

    Added todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -30,5 +30,42 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('Should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Test'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('Should toggle todo completed status', () => {
+      var state = [{
+        id: 1,
+        text: 'Test',
+        completed: false,
+        createdAt: 0,
+        completedAt: undefined
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var res = reducers.todosReducer(df(state), df(action));
+
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toNotBeA('undefined');
+
+      var res = reducers.todosReducer(df(res), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toBeA('undefined');
+
+    });
+  });
+
 
 });
