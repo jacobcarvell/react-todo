73427efcbcc26f4d25eedcd5dc6619551278e3ab
Jacob Carvell <jacobcarvell@gmail.com>
2016-08-29 20:39:30 +1000

    Converted search component to use redux

--- app/tests/components/Search.test.jsx
+++ app/tests/components/Search.test.jsx
@@ -4,32 +4,39 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var Search = require('Search');
+import {Search} from 'Search';
 
 describe('Search', () => {
   it('Should exist', () => {
     expect(Search).toExist();
   });
 
-  it('Should call onSearch with input text', () => {
+  it('Should disptach SET_SEARCH_TEXT on input change', () => {
     var searchText = 'jacob';
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText
+    };
     var spy = expect.createSpy();
-    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+    var search = TestUtils.renderIntoDocument(<Search dispatch={spy} />);
 
     search.refs.searchText.value = searchText;
     TestUtils.Simulate.change(search.refs.searchText);
 
-    expect(spy).toHaveBeenCalledWith(false, searchText);
+    expect(spy).toHaveBeenCalledWith(action);
   });
 
-  it("Should call onSearch with proper checked value", () => {
+  it("Should dipatch TOGGLE_SHOW_COMPLETED when checkbox was checked", () => {
     var spy = expect.createSpy();
-    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var search = TestUtils.renderIntoDocument(<Search dispatch={spy} />);
 
     search.refs.showCompleted.checked = true;
     TestUtils.Simulate.change(search.refs.showCompleted);
 
-    expect(spy).toHaveBeenCalledWith(true, '');
+    expect(spy).toHaveBeenCalledWith(action);
   });
 
 });
