6b9c64a43168ce9a19942a88992acd2fcb9e304f
Jacob Carvell <jacobcarvell@gmail.com>
2016-10-20 11:02:34 +1000

    added jquery reference to app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var $ = require('jquery');
 
 var TodoApp = require('TodoApp');
 var actions = require('actions');
@@ -14,7 +15,7 @@ store.dispatch(actions.startAddTodos());
 $(document).foundation();
 
 // App css
-require('style!css!sass!applicationStyle')
+require('style!css!sass!applicationStyle');
 
 ReactDOM.render(
   <Provider store={store}>
